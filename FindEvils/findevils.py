3dce68804b9ea529331f3c8598b2ed8812e883cc
Heat Miser <heatmiser.contact@gmail.com>
2017-11-17 11:27:23 +0100

    Count processes by name

--- FindEvils/findevils.py
+++ FindEvils/findevils.py
@@ -11,22 +11,28 @@ class FindEvils(common.AbstractWindowsCommand):
         addr_space = utils.load_as(self._config)
         mytasks = tasks.pslist(addr_space)
         for task in mytasks:
-            processes[task.UniqueProcessId] = {}
-            processes[task.UniqueProcessId]["name"] = task.ImageFileName
-            processes[task.UniqueProcessId]["ppid"] = task.InheritedFromUniqueProcessId
+            pid =  "{0}".format(task.UniqueProcessId)
+            name =  "{0}".format(task.ImageFileName)
+            ppid = "{0}".format(task.InheritedFromUniqueProcessId)
+            cmdline = ""
             process_params = task.Peb.ProcessParameters
             if process_params:
-                processes[task.UniqueProcessId]["cmdline"] = process_params.CommandLine
+                cmdline = "{0}".format(process_params.CommandLine)
             else:
-                processes[task.UniqueProcessId]["cmdline"] = "N/A"
+                cmdline = "NA"
+            
+            if name not in processes:
+                processes[name] = []
+            
+            newpp = {}
+            newpp["pid"] = pid
+            newpp["ppid"] = ppid
+            newpp["cmdline"] = cmdline
+            processes[name].append(newpp)
 
         return processes
 
     def render_text(self, outfd, data):
-        outfd.write("PID\tPPID\tNAME\n")
-        for ps in data:
-            outfd.write("{0}\t{1}\t{2}\n".format(ps,
-                                                data[ps]["ppid"],
-                                                data[ps]["name"]
-                                                )
-                )
+        for process in data:
+            outfd.write("%s: seen %s times\n" % (process, len(data[process])))
+
